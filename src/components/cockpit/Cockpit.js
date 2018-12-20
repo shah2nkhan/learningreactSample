7948a2503408874c0952ab0ee6a68ec5ea7f320f
shahanshah khan <shah2nkhan@live.com>
2018-12-21 00:41:31 +0530

    Aux tag

--- src/components/cockpit/Cockpit.js
+++ src/components/cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import classes from './Cockpit.css';
+import Aux from '../../hoc/auxiliary';
 
 const Cockpit = ( props ) => {
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.button , classes.Red].join(' ');
     }
 
     if ( props.persons.length <= 2 ) {
@@ -17,13 +18,14 @@ const Cockpit = ( props ) => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>{ props.appTitle }</h1>
             <p className={assignedClasses.join( ' ' )}>This is really working!</p>
             <button
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
-        </div>
+        
+        </Aux>
     );
 };
 
